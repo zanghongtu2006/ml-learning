bdc5f54caf06d8c7003c8fb06256200ea2a5f8a7
hongtu_zang <zanghongtu2006@gmail.com>
2018-04-12 23:04:41 +0800

    完成全部代码

--- BMLP-TensorFlow/chapter2/2_6/module.py
+++ BMLP-TensorFlow/chapter2/2_6/module.py
@@ -7,7 +7,7 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 import numpy as np
 
-N = 210
+N = 200
 K = 2
 
 centers = [(2, -2), (-2, 1.5), (1.5, -2), (2, 1.5)]
@@ -28,4 +28,32 @@ fig1, ax1 = plt.subplots()
 ax1.scatter(np.asarray(centers).transpose()[0],
             np.asarray(centers).transpose()[1],
             marker='o', s=250)
+# plt.show()
+
+# 损失函数描述和优化循环
+rep_points = tf.reshape(tf.tile(points, [1, K]), [N, K, 2])
+rep_centroids = tf.reshape(tf.tile(centroids, [N, 1]), [N, K, 2])
+sum_squares = tf.reduce_sum(tf.square(rep_points - rep_centroids),
+                            reduction_indices=2)
+best_centroids = tf.argmin(sum_squares, 1)
+print best_centroids
+
+# 停止条件
+did_assignments_chage = tf.reduce_any(tf.not_equal(best_centroids,
+                                                   cluster_assignments))
+
+
+def bucket_mean(data, bucket_ids, num_buckets):
+    total = tf.unsorted_segment_sum(data, bucket_ids, num_buckets)
+    count = tf.unsorted_segment_sum(tf.ones_like(data), bucket_ids, num_buckets)
+    return total / count
+
+
+means = bucket_mean(points, best_centroids, K)
+
+with tf.control_dependencies([did_assignments_chage]):
+    do_updates = tf.group(centroids.assign(means),
+                          cluster_assignments.assign(best_centroids))
+
+print best_centroids
 plt.show()
